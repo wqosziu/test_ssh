1c89c6d94046714795c5d9c135f5fbad28e61a62
wqosziu <jfo64467816672293@163.com>
2021-02-24 23:39:00 +0800

    update

--- ssh/src/com/niit/test/tttt.java
+++ ssh/src/com/niit/test/tttt.java
@@ -4,5 +4,9 @@ public class tttt {
 	public static void main(String[] args) {
 		System.out.println("+++++++++++++");
 	}
-	
+
+	public void test() {
+		System.out.println("0000000000000");
+	}
+
 }
